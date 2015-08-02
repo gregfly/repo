188d2c5f6d65918a1562b202ec4454ec4a244e8b
gregfly <gregfly67@gmail.com>
2015-08-02 23:42:42 +0400

    db fix

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=appdb2',
+    'dsn' => 'mysql:host=localhost;dbname=appdb',
     'username' => 'user',
     'password' => 'user',
     'charset' => 'utf8',
